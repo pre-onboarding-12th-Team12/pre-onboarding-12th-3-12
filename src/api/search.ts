377e91c1bf31e5acab6e8c93ab7b9aa498ba79d1
wlsgus93 <wlsgus63@naver.com>
2023-09-08 23:49:34 +0900

    Test : 배포 테스트

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,11 +1,13 @@
 import Http from 'api/http';
 import { Disease } from 'types';
 
-const URL = 'http://localhost:4000';
+// const URL = 'http://localhost:4000';
 
-const searchHttp = new Http(URL);
+const searchHttp = new Http(
+  `${process.env.REACT_APP_DISEASE_DB || 'http://localhost:4000'}`
+);
 
-export const getKeyword = async (keyword: string) => {
+export const getKeyword = async (keyword: string | undefined) => {
   return await searchHttp.get<Disease[]>('/sick', {
     params: {
       q: keyword,
