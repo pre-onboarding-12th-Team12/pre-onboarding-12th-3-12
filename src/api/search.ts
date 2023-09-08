fa13121272a324e9c933617ed172f8d6e06e934e
plou102 <plou102@naver.com>
2023-09-08 14:06:32 +0900

    Fix: context 오류 수정

--- src/api/search.ts
+++ src/api/search.ts
@@ -6,7 +6,7 @@ const URL = 'http://localhost:4000';
 const searchHttp = new Http(URL);
 
 export const getKeyword = async (keyword: string) => {
-  return await searchHttp.get<Disease[]>('/sick?_limit=10', {
+  return await searchHttp.get<Disease[]>('/sick', {
     params: {
       q: keyword,
     },
